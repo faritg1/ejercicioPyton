991efc18fca1adbedba0b3e7838512f61c076538
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-07-07 11:44:29 -0500

    Se modifico el modulo de clientes

--- clientes.py
+++ clientes.py
@@ -1,6 +1,12 @@
 import core
 import os
 diccCliente = {"data":[]}
+"""
+Metodo para cargar informacion de clientes:
+Si el archivo de recursos no existe lo crea de forma
+automatica con la estructura inicia del diccionario vacio
+diccCliente = {"data":[]}
+"""
 def LoadInfoCliente():
     global diccCliente
     if (core.checkFile("clientes.json")):
@@ -11,7 +17,6 @@ def LoadInfoCliente():
 def MainMenu():
     os.system("clear")
     isCliRun = True
-    os.system("pause")
     os.system("clear")
     print('+','-'*55,'+')
     print("|{:^16}{}{:^15}|".format(' ','ADMINISTRACION DE CLIENTES',' '))
@@ -30,13 +35,43 @@ def MainMenu():
         }
 
         core.crearInfo("clientes.json",data)
-
+        
     elif (opcion == 2):
-        pass
+        os.system("clear")
+        print('+','-'*49,'+')
+        print("|{:^16}{}{:^15}|".format(' ','BUSCADOR DE CLIENTES',' '))
+        print('+','-'*49,'+')
+        cliSearch = input("Ingrese el codigo del cliente a buscar:")
+        for i,item in enumerate(diccCliente["data"]):
+            if cliSearch in item["id"]:
+                print(f'Id cliente : {item["id"]}')
+                print(f'Nombre cliente : {item["nombre"].upper()}')
+                print(f'Email cliente : {item["email"]}')
+        os.system("pause")
     elif (opcion == 3):
-        pass
+        os.system("clear")
+        print('+','-'*49,'+')
+        print("|{:^16}{}{:^15}|".format(' ','EDICION DE CLIENTES',' '))
+        print('+','-'*49,'+')
+        cliSearch = input("Ingrese el codigo del cliente a editar:")
+        for i,item in enumerate(diccCliente["data"]):
+            if cliSearch in item["id"]:
+                item["nombre"] = input("Ingrese en nuevo nombre o presione enter para omitir :") or item["nombre"]
+                item["email"] = input("Ingrese en nuevo email o presione enter para omitir :") or item["email"]
+                core.EditarData("clientes.json",diccCliente)
     elif (opcion == 4):
-        pass
+        os.system("clear")
+        print('+','-'*49,'+')
+        print("|{:^16}{}{:^15}|".format(' ','ELIMINACION DE CLIENTES',' '))
+        print('+','-'*49,'+')
+        cliSearch = input("Ingrese el codigo del cliente a editar:")
+        for i,item in enumerate(diccCliente["data"]):
+            if cliSearch in item["id"]:
+                itemDel = diccCliente["data"].pop(i)
+                core.EditarData("clientes.json",diccCliente)
+                # os.system("pause")
+                # core.crearInfo("clientes.json",itemDel)
+
     elif (opcion == 5):
         isCliRun = False
     if (isCliRun):
